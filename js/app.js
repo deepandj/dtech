9f2cd544f568e299e63f4d0ff7eed96f378c1247
DeepanRaj <deepanraj.selvam@outlook.com>
2020-08-15 17:30:59 +0530

    1st commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var slide = () => {
+    var nav = document.querySelector('.navs');
+    var  burgers = document.querySelector('.burger');
+    var navlinks=document.querySelectorAll('.navs a')
+    var mains=document.querySelector('#main')
+
+    burgers.addEventListener('click',  () => {
+        nav.classList.toggle('nav-active');
+        mains.classList.toggle('mainsmove');
+
+        navlinks.forEach((link,index) => {
+            if(link.style.animation)
+            {
+                link.style.animation='';
+            }
+            else
+            { 
+           
+              link.style.animation=`navlinkslide 0.5s ease forwards ${index/7 + 0.5}s`;
+
+            }
+            
+        });
+    });
+
+    
+}
+slide();
